4da696ab4d7a0d7cab58f5ece4eded538aaadd14
Corey Russ <cruss.seq11@gmail.com>
2019-12-06 12:50:27 -0800

    add playerlife class

--- src/PlayerLife.java
+++ src/PlayerLife.java
@@ -1,4 +1,12 @@
 package src;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class PlayerLife {
+    private static Image img;
+    public void setImg(BufferedImage image) {
+        img = image;
+    }
+    public Image getImg() { return img; }
 }
